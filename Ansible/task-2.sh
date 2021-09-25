fc9463a464c8ae776320a51ec1efe746f5883026
MLH Careem <krm.mrt@gmail.com>
2021-09-25 06:06:44 +0530

    Update task-2.sh

--- Ansible/task-2.sh
+++ Ansible/task-2.sh
@@ -1,2 +1,2 @@
-ansible 10.128.0.17 -m hostname -a"name=student-ID-webserver1"
-ansible 10.128.0.18 -m hostname -a"name=student-ID-webserver2"
+ansible 10.128.0.17 -m hostname -a"name=LT-2021-083-webserver1"
+ansible 10.128.0.18 -m hostname -a"name=LT-2021-083-webserver2"
